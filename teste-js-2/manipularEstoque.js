23a3d3aed1f236d4085be95297a19a30084a0eb7
messiasnycolas <nycolas.prado@mercadolivre.com>
2022-07-21 16:42:00 -0300

    feat: add js-2 challenge solution

---
+++ teste-js-2/manipularEstoque.js
@@ -0,0 +1,42 @@
+let products = [
+    { name: 'Macbook', price: 1300, quantity: 40, colors: ['silver', 'black', 'white'] },
+    { name: 'Iphone', price: 1000, quantity: 50, colors: ['silver', 'red', 'white'] },
+    { name: 'Pendrive', price: 10, quantity: 10, colors: [] },
+    { name: 'Headset', price: 50, quantity: 0, colors: ['black'] },
+    { name: 'Mouse', price: 20, quantity: 5, colors: ['white', 'black', 'blue'] },
+    { name: 'Tablet', price: 500, quantity: 20, colors: ['white', 'black'] },
+    { name: 'USB adaptor', price: 5, quantity: 0, colors: [] },
+    { name: 'Keyboard', price: 30, quantity: 35, colors: ['white'] },
+    { name: 'Gamepad', price: 30, quantity: 25, colors: ['black', 'silver'] },
+    { name: 'Monitor', price: 200, quantity: 3, colors: [] },
+]
+
+// Usando a array acima, crie uma função para cada um dos seguintes requisitos:
+// 1. Adicione um ID exclusivo a cada produto começando em 1.
+// 2. Imprima o nome de cada um dos produtos no console.
+// 3. Imprima no console o produto com o id 3.
+// 4. Imprima no console os produtos com a cor “black”.
+// 5. Imprima no console os produtos que não possuem cor.
+
+//1
+let id = 1;
+console.log('Produtos:'); 
+for (const product of products) {
+    product.id = id++;
+    console.log('\t- ', product.name); //2
+}
+
+//3
+const terceiro = products.find(p => p.id === 3);
+console.log('\nProduto de ID 3:');
+console.log('\t', terceiro);
+
+//4
+const pretos = products.filter(p => p.colors.includes('black'));
+console.log('\nProdutos disponíveis na cor preta:');
+console.log(pretos);
+
+//5
+const semCor = products.filter(p => !p.colors.length);
+console.log('\nProdutos sem cor definida:');
+console.log(semCor);
