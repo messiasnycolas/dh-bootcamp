7915ccc5d00eaeb0742443a8f4107d71a069cf7e
messiasnycolas <nycolas.prado@mercadolivre.com>
2022-07-22 18:26:01 -0300

    feat: add express-2 challenge solution

---
+++ express-2/index.js
@@ -0,0 +1,88 @@
+const products = require('./products');
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const port = 8080;
+
+server.get('/', getProducts);
+server.post('/', createProducts);
+server.put('/:id', updateProduct);
+server.delete('/:id', deleteProduct);
+
+server.listen(port, () => console.log(`Server listening on port ${port}... 👍`));
+
+function getProducts(_req, res) {
+    res.status(200).send(products);
+}
+
+function createProducts(req, res) {
+    const { products: newProducts } = req.body;
+    if (!newProducts || !newProducts.length) res.status(400).send('Body must include at least one product');
+
+    let maxId = products.reduce((acc, curr) => Math.max(acc, curr.id), 1);
+
+    const indexedNewProducts = newProducts.map(product => {
+        if (!product.name || !product.price) return null;
+
+        product.id = ++maxId;
+        return product;
+    }).filter(p => p);
+
+    let response;
+    if (indexedNewProducts.length !== newProducts.length) {
+        response = {
+            message: 'Some products were discarded because information was missing',
+            created: indexedNewProducts,
+        }
+    } else {
+        response = {
+            message: 'Created',
+            created: indexedNewProducts,
+        }
+    }
+
+    products.push(...indexedNewProducts);
+    res.status(201).send(response);
+}
+
+function updateProduct(req, res) {
+    const id = Number(req.params.id);
+    const { body } = req;
+    if (!id || !body) res.status(400).send('ID and Body cannot be empty');
+
+    let indexToUpdate;
+    const productToUpdate = products.find((p, index) => {
+        if (p.id === id) {
+            indexToUpdate = index;
+            return true
+        } else return false
+    });
+    if (!productToUpdate) res.status(404).send('Product not found');
+
+    for (property in productToUpdate) {
+        if (body[property]) {
+            productToUpdate[property] = body[property];
+        }
+    }
+
+    products[indexToUpdate] = productToUpdate;
+    res.status(200).send(productToUpdate);
+}
+
+function deleteProduct(req, res) {
+    const id = Number(req.params.id);
+    if (!id) res.status(400).send('ID cannot be empty and begins at 1');
+
+    let indexToDelete;
+    const productToDelete = products.find((p, index) => {
+        if (p.id === id) {
+            indexToDelete = index;
+            return true
+        } else return false
+    });
+    if (!productToDelete) res.status(404).send('Product not found');
+
+    products.splice(indexToDelete, 1);
+    res.status(204).end();
+}
---
+++ express-2/products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        id: 1,
+        name: 'book',
+        price: 20,
+    },
+    {
+        id: 2,
+        name : 'notebook',
+        price: 10,
+    },
+    {
+        id: 3,
+        name: 'pencil',
+        price: 3,
+    }
+];
+
+module.exports = products;
